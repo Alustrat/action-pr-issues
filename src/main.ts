5422937c23f10d96f6c49c66f40c9df98848cc74
Alustrat <tiers.alustrat@pm.me>
2020-02-27 12:01:53 +0100

    test action available informations

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,32 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import * as Webhooks from '@octokit/webhooks'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const addLabels: string = core.getInput('addlabels')
+    const removeLabels: string = core.getInput('removeLabels')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // eslint-disable-next-line no-console
+    console.log(addLabels)
 
-    core.setOutput('time', new Date().toTimeString())
+    // eslint-disable-next-line no-console
+    console.log(removeLabels)
+
+    // eslint-disable-next-line no-console
+    console.log(github.context.eventName)
+
+    // eslint-disable-next-line no-console
+    console.log(github.context.payload)
+
+    if (github.context.eventName === 'pull_request') {
+      const pushPayload = github.context
+        .payload as Webhooks.WebhookPayloadPullRequestPullRequest
+      // eslint-disable-next-line no-console
+      console.log(pushPayload.title)
+      // eslint-disable-next-line no-console
+      console.log(pushPayload._links)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
